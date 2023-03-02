dd10d4c11326f3065f7c65011827b2e47fa0b5cd
Cleston Moura Filho <clestonmouractm@gmail.com>
2023-03-02 18:33:47 -0300

    Implementação da camada de serviço

--- src/main/java/com/flexpag/paymentscheduler/service/PaymentService.java
+++ src/main/java/com/flexpag/paymentscheduler/service/PaymentService.java
@@ -1,4 +1,67 @@
 package com.flexpag.paymentscheduler.service;
 
+import com.flexpag.paymentscheduler.dto.PaymentDto;
+import com.flexpag.paymentscheduler.dto.PaymentRequestDto;
+import com.flexpag.paymentscheduler.entity.Payment;
+import com.flexpag.paymentscheduler.entity.PaymentStatus;
+import com.flexpag.paymentscheduler.mapper.PaymentMapper;
+import com.flexpag.paymentscheduler.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+
+    public PaymentDto save(PaymentRequestDto paymentRequestDto) {
+        Payment payment = paymentMapper.mapToPayment(paymentRequestDto);
+        paymentRepository.save(payment);
+        return paymentMapper.mapToPaymentDto(payment);
+    }
+
+    public List<PaymentDto> findAllPayments() {
+        return paymentRepository.findAll().stream().map(paymentMapper::mapToPaymentDto).collect(Collectors.toList());
+    }
+
+
+    public PaymentDto checkPayment(Long id) {
+        return paymentMapper.mapToPaymentDto(getPayment(id));
+    }
+
+    public PaymentDto updatePayDate(Long id, LocalDateTime localDateTime) {
+        Payment payment = getPayment(id);
+        payment.setPayDate(localDateTime);
+        paymentRepository.save(payment);
+        return paymentMapper.mapToPaymentDto(payment);
+    }
+
+    public void delete(Long id) {
+        if (getPayment(id).getPaymentStatus() != PaymentStatus.PENDING) {
+            throw new RuntimeException("Cannot delete a PAID payment");
+        }
+        paymentRepository.deleteById(id);
+    }
+
+    public void updatePaymentStatus(List<Payment> payments) {
+        List<Payment> updatedPayments = new ArrayList<>();
+        for (Payment payment : payments) {
+            payment.setPaymentStatus(PaymentStatus.PAID);
+            updatedPayments.add(payment);
+        }
+        paymentRepository.saveAll(updatedPayments);
+    }
+
+    private Payment getPayment(Long id) {
+        return paymentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cannot find payment with Id " + id));
+    }
+
 }
